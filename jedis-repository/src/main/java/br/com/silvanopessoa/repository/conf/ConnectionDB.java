63a5c938e79f44b372f7b245e7297f6a58014173
silvano.pessoa <silvanopessoa@gmail.com>
2014-12-10 23:39:55 -0200

    Configuração de Conexão com Banco

---
+++ jedis-repository/src/main/java/br/com/silvanopessoa/repository/conf/ConnectionDB.java
@@ -0,0 +1,49 @@
+package br.com.silvanopessoa.repository.conf;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * The Class ConnectionDB.
+ */
+public class ConnectionDB {
+	
+	/**
+	 * Gets the connection.
+	 *
+	 * @param host the host
+	 * @param port the port
+	 * @return the connection
+	 */
+	public Jedis getConnection(String host, int port){
+		return new Jedis(host, port);
+	}
+	
+	/**
+	 * Gets the connection.
+	 *
+	 * @param host the host
+	 * @param port the port
+	 * @param timeout the timeout
+	 * @return the connection
+	 */
+	public Jedis getConnection(String host, int port, int timeout){
+		return new Jedis(host, port, timeout);
+	}
+	
+	/**
+	 * Gets the jedis pool.
+	 *
+	 * @param host the host
+	 * @param port the port
+	 * @return the jedis pool
+	 */
+	public JedisPool getJedisPool(String host, int port){
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMinIdle(5);
+		config.setMaxIdle(15);
+		config.setMaxTotal(15);
+		return new JedisPool(config,host,port);
+	}
+}
